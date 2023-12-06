48404bec245d5c5dfb6653c8aa7dce557e2b019d
JurBej <ajurbej2707@g.educaand.es>
2023-12-06 22:06:42 +0100

    mejorado

--- UD5/reto3.js
+++ UD5/reto3.js
@@ -14,11 +14,35 @@ document.addEventListener("DOMContentLoaded", () => {
                 throw new Error(`Error en la solicitud. Código de estado: ${response.status}`);
             }
             let data = await response.json();
+
             //Controlo si data es un array, si no lo es, es que el JSON no existe. Lo hago para controlar la opción 'Selecciona Opcion' del select.
             if (Array.isArray(data)) {
                 data.forEach(element => {
                     let row = document.createElement("tr");
-                    row.innerHTML = `<td><img src="${element.imagen}" alt="${element.espanol}"></td><td>${element.translation.ingles}</td><td>${element.translation.espanol}</td>`;
+                    let img = document.createElement("img");
+
+                    img.src = element.imagen;
+                    img.alt = element.espanol;
+
+                    // Añado un evento click para mostrar los nombres al hacer click en la imagen
+                    img.addEventListener("click", () => {
+                        // Accede a las celdas de la fila y actualiza su contenido
+                        row.cells[1].innerHTML = element.translation.ingles;
+                        row.cells[2].innerHTML = element.translation.espanol;
+                    });
+
+                    let celda1 = document.createElement("td");
+                    let celda2 = document.createElement("td");
+                    let celda3 = document.createElement("td");
+
+                    celda1.appendChild(img);
+                    celda2.innerHTML = ""; // Inicialmente vacío, se actualizará al hacer click en la imagen
+                    celda3.innerHTML = ""; // Inicialmente vacío, se actualizará al hacer click en la imagen
+
+                    row.appendChild(celda1);
+                    row.appendChild(celda2);
+                    row.appendChild(celda3);
+
                     tbody.appendChild(row);
                 });
             }
